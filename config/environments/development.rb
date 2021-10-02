0231fc1650e85acb61b79ac6bec5ef5bce4c578f
Vincent Hardouin <vincenthardouin@icloud.com>
2021-10-02 21:09:53 +0200

    Use devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,12 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025
+  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  root 'home#index'
+
+  get 'home/private'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
