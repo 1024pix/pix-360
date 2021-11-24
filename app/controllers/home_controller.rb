1f1bc900c55aa00f669641f126ce6eaf10ed2f7e
Vincent Hardouin <vincenthardouin@icloud.com>
2021-11-24 21:53:58 +0100

    feat(authenticate_user): Add before_action authenticate_user in application_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [:private]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index; end
 
