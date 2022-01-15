79f15de93beef5977519394ee9448cddd614ca71
Vincent Hardouin <vincenthardouin@icloud.com>
2022-01-15 15:21:49 +0100

    feat(Sign-in): Handle external user when user comes from edit feedback page

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,5 +3,20 @@
 module Users
   class SessionsController < Devise::SessionsController
     skip_before_action :create_encryption_password, :provide_encryption_password, only: %i[new destroy]
+    helper_method :from_feedback?, :feedback_id, :shared_key
+
+    private
+
+    def from_feedback?
+      !!params[:feedback]
+    end
+
+    def feedback_id
+      params[:feedback]
+    end
+
+    def shared_key
+      params[:shared_key]
+    end
   end
 end
