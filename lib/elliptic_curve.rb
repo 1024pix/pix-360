0ae6fe40d360ed1bf521ea0aef2e6c2cb9eda079
Vincent Hardouin <vincenthardouin@icloud.com>
2022-01-16 19:22:20 +0100

    fix(elliptic_curve): Use urlsafe_encode64 for shared_key

--- lib/elliptic_curve.rb
+++ lib/elliptic_curve.rb
@@ -12,7 +12,8 @@ class EllipticCurve
   def shared_key(public_key_string)
     group = OpenSSL::PKey::EC::Group.new(METHOD)
     public_key_point = OpenSSL::PKey::EC::Point.new(group, OpenSSL::BN.new(public_key_string))
-    Base64.encode64 @keys.dh_compute_key(public_key_point)
+    shared_key = @keys.dh_compute_key(public_key_point)
+    Base64.urlsafe_encode64 shared_key
   end
 
   def public_key
