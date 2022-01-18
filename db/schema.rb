6a122d330b03094d963aea16f87bad9adb231942
Vincent Hardouin <vincenthardouin@icloud.com>
2022-01-18 10:55:53 +0100

    Add migration to add columns for user name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_131235) do
+ActiveRecord::Schema.define(version: 2022_01_18_094328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2022_01_10_131235) do
     t.boolean "must_change_password", default: true
     t.string "private_key"
     t.string "public_key"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
