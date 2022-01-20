d5d06b19e7cf390c160d9633dc16df64426a2dc9
HugoPauthier <hugo.pauthier@gmail.com>
2022-01-20 10:48:38 +0100

    Added new field in feedbacks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_094328) do
+ActiveRecord::Schema.define(version: 2022_01_20_092459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_094328) do
     t.integer "respondent_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_filled", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
