86411be73be60f0c24fe4366d47c6683790d46c6
Vincent Hardouin <vincenthardouin@icloud.com>
2022-01-21 09:19:41 +0100

    feat(feedbacks_mailer): Add name to e-mail address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: email_address_with_name('notification@pix.fr', 'Pix 360')
   layout 'mailer'
 end
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class FeedbackMailer < ApplicationMailer
-  default from: 'feedbacks@example.com'
+  default from: email_address_with_name('notification@pix.fr', 'Pix 360')
 
   def feedback_email
     @email = params[:email]
