c1fde089c74fbae6c4514b9af025712c540fde88
HugoPauthier <hugo.pauthier@gmail.com>
2022-01-31 16:22:52 +0100

    Fixed routing error

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def not_found
-    render template: 'errors/not_found', layout: 'layouts/error', status: :not_found
+    render layout: 'error', status: :not_found
   end
 
   def internal_server
-    render template: 'errors/not_found', layout: 'layouts/error', status: :internal_server_error
+    render layout: 'error', status: :internal_server_error
   end
 
   def unprocessable
-    render template: 'errors/not_found', layout: 'layouts/error', status: :unprocessable_entity
+    render layout: 'error', status: :unprocessable_entity
   end
 
   def unacceptable
-    render template: 'errors/not_found', layout: 'layouts/error', status: :not_acceptable
+    render layout: 'error', status: :not_acceptable
   end
 end
