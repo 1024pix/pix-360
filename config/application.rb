7f2b74e9ef5816c8883b85767b66d9db49dd32a1
HugoPauthier <hugo.pauthier@gmail.com>
2022-01-31 10:41:08 +0100

    Add controller & views to handle error pages

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Pix360
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.exceptions_app = routes
 
     # Configuration for the application, engines, and railties goes here.
     #
