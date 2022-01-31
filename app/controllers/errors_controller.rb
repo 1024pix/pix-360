7ea329fb0e982b6b5740254c5ed0b32b4c5b232e
Hugo Pauthier <hugo.pauthier@gmail.com>
2022-01-31 20:24:28 +0100

    Removed useless method

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,16 +6,4 @@ class ErrorsController < ApplicationController
   def not_found
     render layout: 'error', status: :not_found
   end
-
-  def internal_server
-    render layout: 'error', status: :internal_server_error
-  end
-
-  def unprocessable
-    render layout: 'error', status: :unprocessable_entity
-  end
-
-  def unacceptable
-    render layout: 'error', status: :not_acceptable
-  end
 end
