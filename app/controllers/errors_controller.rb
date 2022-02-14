4b0741fe45c9789ba2754aafaf996f666dab1871
Vincent Hardouin <vincenthardouin@icloud.com>
2022-02-14 15:23:04 +0100

    tech: Remove useless request in errors_controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :create_encryption_password, :provide_encryption_password
 
   def not_found
     render layout: 'error', status: :not_found
