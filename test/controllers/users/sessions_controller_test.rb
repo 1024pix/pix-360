33162e97d972f3e320ded72a918c1cdfbbb8cfac
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-06-20 16:34:16 +0200

    chore: change devise destroy method to get to allow redirect_to

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test '#destroy: should sign_out user ' do
     sign_in users(:two)
-    delete destroy_user_session_url
+    get destroy_user_session_url
     assert_redirected_to root_url
   end
 end
